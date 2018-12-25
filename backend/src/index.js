fa842a1efbc44ec455157340edf9f3470375213a
Douglas Matoso <doug2k1@gmail.com>
2018-12-25 21:50:58 -0200

    GraphQL Yoga server.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  s => {
+    console.log(`Server running on http://localhost:${s.port}`);
+  }
+);
